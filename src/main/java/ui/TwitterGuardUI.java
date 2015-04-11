bde12a7bfcf20dec27072e4e6c943c41d61aae1f
blostic <piotr.skibiak@gmail.com>
2015-04-11 19:43:46 +0200

    Add menu, styling

--- src/main/java/ui/TwitterGuardUI.java
+++ src/main/java/ui/TwitterGuardUI.java
@@ -1,57 +1,26 @@
 package ui;
 
-import ui.resources.TwitterGuardsIcons;
+import ui.menu.TwitterHeader;
+import ui.views.OverviewView;
 
-import com.vaadin.annotations.PreserveOnRefresh;
+import com.vaadin.annotations.JavaScript;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.tapio.googlemaps.GoogleMap;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.Image;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Window;
 
+@JavaScript({ "http://code.jquery.com/jquery-2.1.3.min.js" })
 @Theme("mytheme")
 //@PreserveOnRefresh
 public class TwitterGuardUI extends UI {
-	private final class WindowExtension extends Window {
-		private static final long serialVersionUID = 1L;
-		
-		public WindowExtension() {
-			super();
-			VerticalLayout vl = new VerticalLayout();
-			Label label = new Label("test222wee");
-			vl.addComponent(label);
-			setContent(vl);
-		}
-	}
-
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void init(VaadinRequest request) {
-		VerticalLayout content = new VerticalLayout();
-//		content.setSizeFull();
-		GoogleMap googleMap = new GoogleMap("");
-		googleMap.setWidth("800px");
-		googleMap.setHeight("400px");
-		Button b = new Button("Show window");
-		b.addClickListener(new ClickListener() {
-			
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void buttonClick(ClickEvent event) {
-				UI.getCurrent().addWindow(new WindowExtension());
-			}
-		});
-		content.addComponents(googleMap, new Label("Piotr"),new Label("Piotr2"), b);
-		Image image = new Image("image", TwitterGuardsIcons.MAIN_VIEW_ICON.getFileResource());
-		content.addComponent(image);
-		setContent(content);
+		
+		TwitterHeader header = new TwitterHeader();
+		ContentWrapper contentWrapper = new ContentWrapper(header, new OverviewView());
+		
+		setContent(contentWrapper);
 	}
+
 }
---
+++ src/main/java/ui/views/TwitterGuardsView.java
@@ -0,0 +1,9 @@
+package ui.views;
+
+import com.vaadin.ui.VerticalLayout;
+
+public class TwitterGuardsView extends VerticalLayout {
+
+	private static final long serialVersionUID = 1L;
+
+}
