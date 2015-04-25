f7d61e23352e8f01928fa0d9f49943c8dd76f7c2
blostic <piotr.skibiak@gmail.com>
2015-04-25 19:08:26 +0200

    Initiate editor

---
+++ src/main/java/ui/utils/CustomeTextField.java
@@ -0,0 +1,16 @@
+package ui.utils;
+
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+
+public class CustomeTextField extends HorizontalLayout {
+
+	private static final long serialVersionUID = 1L;
+	
+	private TextField field = new TextField();
+	
+	public CustomeTextField(String descripiton, String initialValue){ 
+		
+	}
+	
+}
