14e6493095704970ed9ae7821e55a00a00bae602
blostic <piotr.skibiak@gmail.com>
2015-04-25 20:26:04 +0200

    Add basic info step

--- src/main/java/ui/timeline/editor/EditorView.java
+++ src/main/java/ui/timeline/editor/EditorView.java
@@ -1,9 +1,24 @@
 package ui.timeline.editor;
 
+import ui.timeline.editor.steps.BasicConfig;
+
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Layout;
 import com.vaadin.ui.VerticalLayout;
 
 public class EditorView extends VerticalLayout {
 
 	private static final long serialVersionUID = 1L;
+	
+	private Layout currentEditorContent;
 
+	private StepsMenu menu;
+	
+	public EditorView() {
+		menu = new StepsMenu();
+		currentEditorContent = new BasicConfig();
+		addComponents(menu, currentEditorContent);
+		setComponentAlignment(currentEditorContent, Alignment.MIDDLE_CENTER);
+	}
+	
 }
--- src/main/java/ui/utils/CustomeTextField.java
+++ src/main/java/ui/utils/CustomeTextField.java
@@ -1,16 +1,36 @@
 package ui.utils;
 
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 
 public class CustomeTextField extends HorizontalLayout {
 
 	private static final long serialVersionUID = 1L;
 	
-	private TextField field = new TextField();
+	private TextField textField = new TextField();
+	
+	private String DEFAULT_WIDTH = "300px";
+	private String DEFAULT_HEIGHT = "40px";
 	
 	public CustomeTextField(String descripiton, String initialValue){ 
+		customizeTextField();
+		Label descripitonLabel = new Label(descripiton);
+		textField.setInputPrompt(initialValue);
+
+		addComponents(descripitonLabel, textField);
 		
+		setComponentAlignment(descripitonLabel, Alignment.MIDDLE_RIGHT);
+		setComponentAlignment(textField, Alignment.MIDDLE_RIGHT);
+		
+		setSpacing(true);
+	}
+
+	private void customizeTextField() {
+		textField.setWidth(DEFAULT_WIDTH);
+		textField.setHeight(DEFAULT_HEIGHT);
+		setStyleName("custome-text-field");
 	}
 	
 }
