e88c051465776be82bcb56970f81f6eb01a7c02c
blostic <piotr.skibiak@gmail.com>
2015-04-25 17:55:29 +0200

    Timeline styling

--- src/main/java/ui/timeline/TimelineComponent.java
+++ src/main/java/ui/timeline/TimelineComponent.java
@@ -13,10 +13,11 @@ public class TimelineComponent extends VerticalLayout {
     public TimelineComponent(){
         setId("cd-timeline");
         setStyleName("cd-container");
+        notes.add(new TimelineNote());
         for (int i = 0; i < 10; i++){
-            notes.add(new TimelineNote("asd", "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum \" +\n" +
-                    "                \"provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus \" +\n" +
-                    "                \"minus veritatis qui ut</p>"));
+            notes.add(new TimelineNote("Przykładowy tytuł kampani", "<p>Lorem ipsum dolor sit amet, qualisque torquatos signiferumque"
+            		+ " ex nec, eu ius duis clita nonumy. Doctus indoctum in cum, zril tritani malorum an qui, te mei sint docendi."
+            		+ " Ea sit dicta ludus eirmod, tale facilis id vel. His timeam epicuri praesent ut. Eos id dico consequat.</p>"));
         }
         notes.forEach(note->addComponent(note));
         
