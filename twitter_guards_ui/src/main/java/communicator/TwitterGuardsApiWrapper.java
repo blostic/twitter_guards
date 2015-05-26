647d37dfca0ff500c1ca51b19087807a6afaa367
radoslawdyrda <raddyr@gmail.com>
2015-05-26 18:06:32 +0200

    REST integrated view

--- twitter_guards_ui/src/main/java/communicator/TwitterGuardsApiWrapper.java
+++ twitter_guards_ui/src/main/java/communicator/TwitterGuardsApiWrapper.java
@@ -13,8 +13,10 @@ import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -27,13 +29,15 @@ public class TwitterGuardsApiWrapper {
 	public void sendHttpRequest(Campaign campaign) throws ClientProtocolException, IOException {
 		HttpClient httpclient = HttpClients.createDefault();
 		HttpPost httppost = new HttpPost(PropertiesManager.getProperty("twitter_service_address") + "rest/twitterCampaigns/add");
-		ObjectMapper mapper = new ObjectMapper();
-		String json = mapper.writeValueAsString(campaign);
-		
-		List<NameValuePair> params = new ArrayList<NameValuePair>(1);
-		params.add(new BasicNameValuePair("camapignName", campaign.getTitle()));
-		params.add(new BasicNameValuePair("pollingText", new Gson().toJson(campaign.getKeywords())));
-		httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+//		ObjectMapper mapper = new ObjectMapper();
+//		String json = mapper.writeValueAsString(campaign);
+
+		StringEntity params = new StringEntity(
+				"{\"camapignName\": \"" + campaign.getTitle() + "\",\"pollingText\":" + new Gson().toJson(campaign.getKeywords()) + "}"
+		);
+		httppost.addHeader("content-type", "application/json");
+		httppost.addHeader("Accept", "application/json");
+		httppost.setEntity(params);
 
 		HttpResponse response = httpclient.execute(httppost);
 		HttpEntity entity = response.getEntity();
