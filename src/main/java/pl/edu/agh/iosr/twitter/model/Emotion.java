c8f17fa3952398ef0835cdda43160522ace881e2
Piotr Skibiak <piotr.skibiak@gmail.com>
2015-05-27 11:26:45 +0200

    Fix emotion model

--- src/main/java/pl/edu/agh/iosr/twitter/model/Emotion.java
+++ src/main/java/pl/edu/agh/iosr/twitter/model/Emotion.java
@@ -1,30 +1,36 @@
 package pl.edu.agh.iosr.twitter.model;
 
 public enum Emotion {
-    SUPER_POSITIVE("Super positive"),
-    POSITIVE("Positive"),
-    NEUTRAL("Neutral"),
-    NEGATIVE("Negative"),
-    SUPER_NEGATIVE("Super negative"),
-    UNRECOGNIZED("Unrecognized");
-    
-    private String value;
-    
-    private Emotion(String value) {
-    	this.value = value;
-    }
-    
-    @Override
-    public String toString() {
-    	return value;
-    }
+	SUPER_POSITIVE("Super positive"), POSITIVE("Positive"), NEUTRAL("Neutral"), NEGATIVE(
+			"Negative"), SUPER_NEGATIVE("Super negative"), UNRECOGNIZED(
+			"Unrecognized");
 
-    public static Emotion getValue(int rank){
-        switch (rank) {
-            case 1: return NEGATIVE;
-            case 2: return NEUTRAL;
-            case 3: return POSITIVE;
-            default: return UNRECOGNIZED;
-        }
-    }
+	private String value;
+
+	private Emotion(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return value;
+	}
+
+	public static Emotion getValue(int rank) {
+		switch (rank) {
+		case 0:
+			return SUPER_NEGATIVE;
+		case 1:
+			return NEGATIVE;
+		case 2:
+			return NEUTRAL;
+		case 3:
+			return POSITIVE;
+		case 4:
+			return SUPER_POSITIVE;
+		default:
+			return UNRECOGNIZED;
+		}
+	}
+	
 }
